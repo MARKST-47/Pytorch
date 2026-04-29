b85e8744d686b63903ef89bd6bed44505437e000
Mark <markstephenthomas10@gmail.com>
2026-04-29 20:47:22 +0200

    trained model -u

--- alexnet.py
+++ alexnet.py
@@ -3,47 +3,12 @@ import torchvision
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
-
-transform = transforms.Compose(
-    [
-        transforms.Resize((227, 227)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ]
-)
-
-batch_size = 128
-
-trainset = torchvision.datasets.CIFAR10(
-    root="./data", train=True, download=True, transform=transform
-)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=batch_size, shuffle=True, num_workers=2
-)
-
-testset = torchvision.datasets.CIFAR10(
-    root="./data", train=False, download=True, transform=transform
-)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=batch_size, shuffle=False, num_workers=2
-)
-
-classes = (
-    "plane",
-    "car",
-    "bird",
-    "cat",
-    "deer",
-    "dog",
-    "frog",
-    "horse",
-    "ship",
-    "truck",
-)
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class AlexNet(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes=10):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0)
         self.conv2 = nn.Conv2d(96, 256, kernel_size=5, padding=2)
@@ -60,12 +25,12 @@ class AlexNet(nn.Module):
             nn.Dropout(p=0.5),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
-            nn.Linear(4096, 10),
+            nn.Linear(4096, num_classes),
         )
 
     def forward(self, x):
-        x = self.pool(F.relu(self.lrn(self.conv1(x))))
-        x = self.pool(F.relu(self.lrn(self.conv2(x))))
+        x = self.pool(self.lrn(F.relu(self.conv1(x))))
+        x = self.pool(self.lrn(F.relu(self.conv2(x))))
         x = F.relu(self.conv3(x))
         x = F.relu(self.conv4(x))
         x = self.pool(F.relu(self.conv5(x)))
@@ -73,3 +38,91 @@ class AlexNet(nn.Module):
         x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
+
+
+def test_and_show_examples(model, test_loader, device, classes):
+    model.eval()
+    dataiter = iter(test_loader)
+    images, labels = next(dataiter)
+    images_gpu = images.to(device)
+
+    with torch.no_grad():
+        outputs = model(images_gpu)
+        _, predicted = torch.max(outputs, 1)
+
+    plt.figure(figsize=(15, 5))
+    for i in range(5):
+        plt.subplot(1, 5, i + 1)
+        img = images[i].numpy().transpose((1, 2, 0))
+        img = np.array([0.229, 0.224, 0.225]) * img + np.array([0.485, 0.456, 0.406])
+        img = np.clip(img, 0, 1)
+        plt.imshow(img)
+        color = "green" if predicted[i] == labels[i] else "red"
+        plt.title(f"P: {classes[predicted[i]]}\nA: {classes[labels[i]]}", color=color)
+        plt.axis("off")
+    plt.show()
+
+
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+
+    transform = transforms.Compose(
+        [
+            transforms.Resize((227, 227)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+
+    trainset = torchvision.datasets.CIFAR10(
+        root="./data", train=True, download=True, transform=transform
+    )
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=128, shuffle=True, num_workers=2
+    )
+
+    testset = torchvision.datasets.CIFAR10(
+        root="./data", train=False, download=True, transform=transform
+    )
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=128, shuffle=False, num_workers=2
+    )
+
+    classes = (
+        "plane",
+        "car",
+        "bird",
+        "cat",
+        "deer",
+        "dog",
+        "frog",
+        "horse",
+        "ship",
+        "truck",
+    )
+
+    model = AlexNet().to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(
+        model.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005
+    )
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
+
+    # Training Loop
+    for epoch in range(5):  # Starting with 5 epochs to test
+        model.train()
+        running_loss = 0.0
+        for images, labels in trainloader:
+            images, labels = images.to(device), labels.to(device)
+            optimizer.zero_grad()
+            loss = criterion(model(images), labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+        scheduler.step()
+        print(f"Epoch {epoch + 1} Loss: {running_loss / len(trainloader):.4f}")
+
+    print("Training Complete!")
+    test_and_show_examples(model, testloader, device, classes)
