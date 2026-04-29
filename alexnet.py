09e6b1fde4ae31498a6db09e648d939cbbdf6254
Mark <markstephenthomas10@gmail.com>
2026-04-29 20:22:35 +0200

    alexnet class

--- alexnet.py
+++ alexnet.py
@@ -40,3 +40,36 @@ classes = (
     "ship",
     "truck",
 )
+
+
+class AlexNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0)
+        self.conv2 = nn.Conv2d(96, 256, kernel_size=5, padding=2)
+        self.conv3 = nn.Conv2d(256, 384, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(384, 384, kernel_size=3, padding=1)
+        self.conv5 = nn.Conv2d(384, 256, kernel_size=3, padding=1)
+        self.lrn = nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2)
+        self.pool = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 10),
+        )
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.lrn(self.conv1(x))))
+        x = self.pool(F.relu(self.lrn(self.conv2(x))))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = self.pool(F.relu(self.conv5(x)))
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
