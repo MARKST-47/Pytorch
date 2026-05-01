93869d7105eb89019a12dfdeb02285938c18ba52
Mark <markstephenthomas10@gmail.com>
2026-05-01 23:45:56 +0200

    dataset -u

--- alexnet.py
+++ alexnet.py
@@ -76,14 +76,14 @@ if __name__ == "__main__":
     )
 
     trainset = torchvision.datasets.CIFAR10(
-        root="./data", train=True, download=True, transform=transform
+        root="./data", train=True, download=False, transform=transform
     )
     trainloader = torch.utils.data.DataLoader(
         trainset, batch_size=128, shuffle=True, num_workers=2
     )
 
     testset = torchvision.datasets.CIFAR10(
-        root="./data", train=False, download=True, transform=transform
+        root="./data", train=False, download=False, transform=transform
     )
     testloader = torch.utils.data.DataLoader(
         testset, batch_size=128, shuffle=False, num_workers=2
